48a9fa01ac9b015a0d345396a97d27b284305c62
tomtom12065 <100985000+tomtom12065@users.noreply.github.com>
2023-11-08 01:51:00 +0000

    did backend

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -12,8 +12,10 @@ const getState = ({ getStore, getActions, setStore }) => {
 					background: "white",
 					initial: "white"
 				}
-			]
+			],
+			contacts: []
 		},
+
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
@@ -37,9 +39,72 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
-			}
-		}
-	};
-};
+			},
+			createContact: (fullname, email, address, phone) => {
+				fetch(" https://playground.4geeks.com/apis/fake/contact/tomtom", {
+					method: 'POST', // or 'PUT', 'GET', 'DELETE', etc.
+					headers: {
+						'Content-Type': 'application/json',
+						// Additional headers
+					},
+					body: JSON.stringify({
+						fullname: fullname,
+						email: email,
+						address: address,
+						phone: phone,
+						//  agenda_slug:"tomtom"
+					})
+				})
+				setStore({
+					fullname: fullname,
+					email: email,
+					address: address,
+					phone: phone
+				})
+			},
+			getContacts: () => {
+				return getStore().contacts
+			},
+			deleteContact: (contact) => {
+				const store = getStore();
+				// Filter the contacts to create a new array without the contact with the given email
+				const updatedContacts = store.contacts.filter(contact => contact.email !== contact.email);
 
+				// Update the store with the new contacts array
+				setStore({ contacts: updatedContacts });
+				fetch(" https://playground.4geeks.com/apis/fake/contact/{contact_id}",
+					{
+						method: "DELETE",
+						headers: {
+							'Content-Type': 'application/json',
+							body: JSON.stringify(contact.id)
+						}
+					})
+			},
+			updatedContacts: (contact) => {
+				fetch("https://playground.4geeks.com/apis/fake/contact/{contact_id}", {
+					method: 'PUT', // or 'PUT', 'GET', 'DELETE', etc.
+					headers: {
+						'Content-Type': 'application/json',
+						// Additional headers
+					},
+					body: JSON.stringify({
+						fullname: contact.fullname,
+						email: contact.email,
+						address: contact.address,
+						phone: contact.phone,
+						//  agenda_slug:"tomtom"
+					})
+				})
+				setStore({
+					fullname: contact.fullname,
+					email: contact.email,
+					address: contact.address,
+					phone: contact.phone
+				})
+			}
+
+		},
+	};
+}
 export default getState;
