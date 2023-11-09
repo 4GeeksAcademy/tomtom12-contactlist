a58e5bb53383eeb253b138a1c914f50a92f93a1f
tomtom12065 <100985000+tomtom12065@users.noreply.github.com>
2023-11-09 00:22:04 +0000

    heeelp

--- src/js/component/addcontact.js
+++ src/js/component/addcontact.js
@@ -1,10 +1,76 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
 
-import { Context } from "../store/appContext";
-export const AddContacts = () => {
+import '../../styles/contactform.css';
+function ContactForm() {
+  const [fullname, setFullname] = useState('');
+  const [email, setEmail] = useState('');
+  const [address, setAddress] = useState('');
+  const [phone, setPhone] = useState('');
+  
+  const createContact = () => {
+    
 
-    const { store, actions } = useContext(Context);
+    // Your fetch code here
+    fetch("https://playground.4geeks.com/apis/fake/contact/tomtom", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        fullname: fullname,
+        email: email,
+        address: address,
+        phone: phone,
+      }),
+    })
+      .then((response) => {
+        // Handle the response as needed
+        console.log(response);
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error(error);
+      });
+  };
 
-    return (<div></div>)
-};
+  return (
+    <div className='contactform'>
+      <h2>Create Contact</h2>
+      <div>
+        <label>Full Name:</label>
+        <input
+          type="text"
+          value={fullname}
+          onChange={(e) => setFullname(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Address:</label>
+        <input
+          type="text"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Phone:</label>
+        <input
+          type="tel"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+      </div>
+      <button onClick={createContact}>Create Contact</button>
+    </div>
+  );
+}
+
+export default ContactForm;
--- src/js/component/contacts.js
+++ src/js/component/contacts.js
@@ -1,10 +1,39 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import { Context } from "../../store/appContext.js"; 
+=
+function ContactList() {
+  
+  const { getContacts, deleteContact, updateContact } = useContext(Context); 
 
-import { Context } from "../store/appContext";
-export const AddContacts = () => {
+  // const contacts = getContacts();
+  const { store, actions } = useContext(Context);
+  
 
-    const { store, actions } = useContext(Context);
+  const handleDeleteContact = (contact) => {
+    action.deleteContact(contact);
+   
+  };
 
-    return (<div></div>)
-};
+  const handleUpdateContact = (contact) => {
+    action.updateContact(contact);
+  
+  };
+
+  return (
+    <div>
+      <h2>Contact List</h2>
+      {contacts.map((contact) => (
+        <div key={contact.id} className="contact-card">
+          <h3>{contact.fullname}</h3>
+          <p>Email: {contact.email}</p>
+          <p>Address: {contact.address}</p>
+          <p>Phone: {contact.phone}</p>
+          <button onClick={() => handleDeleteContact(contact)}>Delete</button>
+          <button onClick={() => handleUpdateContact(contact)}>Update</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default ContactList;
